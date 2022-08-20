87d42dc0ab0c2adea5a1afbad6f2bd3c23ee6b1b
smudgeapp <93826778+smudgeapp@users.noreply.github.com>
2022-08-21 00:37:38 +0500

    Delete app directory

--- app/src/main/java/com/smudge/cheat_o_meter/DetectionListener.java
+++
@@ -1,9 +0,0 @@
-package com.smudge.cheat_o_meter;
-
-import org.opencv.core.Rect;
-
-public interface DetectionListener {
-    void FaceDetected(Rect rect, double adjx, double adjy);
-    void EyesDetected(Rect facerect, Rect eyesrect);
-
-}
